ada70a8851709b412a66f42b3e030b1a2921a85d
Andrei Starodubtsev <dub4ek@gmail.com>
2016-02-10 00:07:20 +0400

    fix review bugs

--- First project/src/main/js/controller/CreateEventPageController.js
+++ First project/src/main/js/controller/CreateEventPageController.js
@@ -16,13 +16,10 @@
         vm.startDateTime = '';
         vm.eventNameText = '';
         vm.eventHostText = '';
-        vm.addedFriendList = [];
         vm.countryText = '';
         vm.cityText = '';
         vm.addressText = '';
         vm.currentStep = 0;
-        vm.friendNameText = '';
-        vm.friendEmailText = '';
         vm.incorrectDate = false;
 
 
@@ -38,7 +35,6 @@
             event.host = vm.eventHostText;
             event.startDate = vm.startDateTime.toUTCString();
             event.endDate = vm.endDateTime.toUTCString();
-            event.friendList = vm.addedFriendList.concat();
             event.country = vm.countryText;
             event.city = vm.cityText;
             event.address = vm.addressText;
@@ -54,58 +50,20 @@
         }
 
         vm.completeButton_clickHandler = function () {
-            if (vm.locationForm.$valid) {
-                saveEvent();
-            }
-        }
-
-        vm.nextButton_clickHandler = function () {
-            var validResult = false;
-
             function checkEnteredDates() {
                 return new Date(vm.startDateTime) < new Date(vm.endDateTime)
             }
 
-            if (vm.currentStep === 0) {
-                validResult = vm.describeEventForm.$valid && checkEnteredDates();
-
-                if (vm.describeEventForm.$valid && !checkEnteredDates()) {
-                    $mdToast.show(
-                        $mdToast.simple()
-                            .textContent('End date should be greater, than start date')
-                            .parent($document[0].querySelector('#toastPositionEvent'))
-                            .position('bottom right')
-                            .hideDelay(6000)
-                    );
-                }
-            } else if (vm.currentStep === 1) {
-                validResult = vm.addedFriendList.length > 0;
-
-                if (!validResult) {
-                    $mdToast.show(
-                        $mdToast.simple()
-                            .textContent('Please, add at least one friend')
-                            .parent($document[0].querySelector('#toastPositionFriends'))
-                            .position('bottom right')
-                            .hideDelay(6000)
-                    );
-                }
-            }
-
-            if (validResult) {
-                vm.currentStep += 1;
-            }
-        }
-
-        vm.prevButton_clickHandler = function () {
-            vm.currentStep -= 1;
-        }
-
-        vm.addFriendButton_clickHandler = function () {
-            if (vm.inviteFriendsForm.$valid) {
-                vm.addedFriendList.push(new FriendData(vm.friendNameText, vm.friendEmailText));
-                vm.friendNameText = '';
-                vm.friendEmailText = '';
+            if (vm.describeEventForm.$valid && !checkEnteredDates()) {
+                $mdToast.show(
+                    $mdToast.simple()
+                        .textContent('End date should be greater, than start date')
+                        .parent($document[0].querySelector('#toastPositionEvent'))
+                        .position('top right')
+                        .hideDelay(6000)
+                );
+            } else {
+                saveEvent();
             }
         }
     }
